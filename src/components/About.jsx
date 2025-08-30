d2a17360bcb15c9645e79eb38741b045fec07078
saisushreet <saisushreet@gmail.com>
2025-08-30 17:29:46 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-green-600 font-semibold text-xl">About</h1>
+        <p>
+          Hello, I'm <span className="font-semibold">Sai Sushreeta Lenka</span>,
+          a results-oriented Computer Science graduate with a passion for
+          technology and problem-solving. I enjoy creating impactful and
+          visually appealing software solutions while continuously improving
+          my technical and communication skills.
+        </p>
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+
+        <strong>Silicon University (Bhubaneswar, Odisha)</strong> —
+        B.Tech in Computer Science Engineering (2022 - 2025)
+        <br />
+
+        <strong>KK Polytechnic (JUT Board, Dhanbad, Jharkhand)</strong> —
+        Diploma in Civil Engineering (2019 - 2022)
+
+        <br />
+        <strong>Kendriya Vidyalaya Koliwada (Mumbai, Maharashtra)</strong> —
+        Matriculation X (2018 – 2019)
+        <br />
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <p>
+          <strong>Technical Skills:</strong> Python, Java, HTML, CSS, ReactJS,
+          MongoDB, MySQL, SAP ABAP <br />
+          <strong>Soft Skills:</strong> Problem-solving, Teamwork, Communication,
+          Adaptability, Leadership
+        </p>
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Projects
+        </h1>
+        <div>
+          <h2 className="font-semibold">Pizza Nest (E-Commerce Website)</h2>
+
+          Built using HTML, CSS, ReactJS, and MongoDB <br />
+          Developed a responsive platform for pizza ordering <br />
+          Integrated secure payment options <br />
+          Implemented a dynamic menu with real-time updates <br />
+
+        </div>
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Certification & Training
+        </h1>
+
+        SAP ABAP Cloud Developer (EME) — Mar 2025 <br />
+
+        Mastering React JS (Bigbinary Academy) — Jun–Aug 2024 <br />
+
+        MERN Stack Intern (Preplabs) — Jun–Jul 2023 <br />
+
+        Cyber Security & Privacy (NPTEL, IIT Madras)
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">Additional</h1>
+        <p>
+          <strong>Languages:</strong> English (Fluent), Hindi (Fluent),
+          Odia (Conversational) <br />
+          <strong>Hobbies:</strong> Playing musical instruments, gymming,
+          riding & exploring new places
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
